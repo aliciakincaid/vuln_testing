bf3d7111b6a7fc5bfeac1c7c050edb88f276e4f8
Kincaid, Alicia <alicia.kincaid@accenture.com>
2021-05-17 10:33:17 -0500

    First commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.Scanner;
+import java.io.*;
+
+class Main{
+	public static void main(String[] args){
+		
+		OSCommand myobj = new OSCommand();
+		myobj.OSCommand_main();
+		OSCommand2 myobj2 = new OSCommand2();
+		myobj2.OSCommand_main();
+		OSCommand3 myobj3 = new OSCommand3();
+		myobj3.OSCommand_main();
+		OSCommand4 myobj4 = new OSCommand4();
+		myobj4.OSCommand_main();
+		OSCommand5 myobj5 = new OSCommand5();
+		myobj5.OSCommand_main();
+	}
+
+}
+
---
+++ OSCommand.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.util.Scanner;
+import java.io.*;
+
+
+public class OSCommand{
+		public static void OSCommand_main(){
+			/* TEST 1*/
+			System.out.println("Enter OS Command: ");
+			
+			// get os command from user
+			Scanner obj = new Scanner(System.in);
+			String command = obj.nextLine();
+			
+			
+			try{
+				Process p = Runtime.getRuntime().exec("cmd /c " + command);
+				BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));		
+				String s = null;
+				while((s = stdInput.readLine()) != null){
+					System.out.println(s);
+				}
+			}
+			catch (Exception ex){
+					ex.printStackTrace();
+			}
+			// Runtime.getRunTime().exec(command);
+			
+			
+			/* TEST 2 */
+			// String password2 = "heyyo1234"
+			
+			
+			String password = "yoyo1234";
+			
+	}
+}
+	
